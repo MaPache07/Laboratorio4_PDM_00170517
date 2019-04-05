33d62214cbae24053a20269feeb3c5af17c49b0c
MaPache07 <00170517@uca.edu.sv>
2019-04-05 15:42:29 -0600

    Funciona!!

---
+++ app/src/main/java/com/dondyprax/fakecine/activities/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.dondyprax.fakecine.activities
+
+import android.net.Network
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import com.dondyprax.fakecine.R
+import com.dondyprax.fakecine.adapters.MovieAdapter
+import com.dondyprax.fakecine.network.NetworkUtils
+import com.dondyprax.fakecine.pojos.Movie
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private var movieList: ArrayList<Movie> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initRecyclerView()
+        initSearchButton()
+    }
+
+    fun initRecyclerView(){
+        viewManager = LinearLayoutManager(this)
+
+        movieAdapter = MovieAdapter(movieList, {movieItem: Movie -> movieItemClicked(movieItem)})
+
+        movie_list_rv.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = movieAdapter
+        }
+    }
+
+    fun initSearchButton() = add_movie_btn.setOnClickListener{
+        if (!movie_name_et.text.toString().isEmpty()){
+            FetchMovie().execute(movie_name_et.text.toString())
+        }
+    }
+
+    private fun movieItemClicked(movie: Movie){
+        /*movieList.add(movie)
+        movieAdapter.changeList(movieList)*/
+    }
+
+    fun addMovieToList(movie: Movie){
+        movieList.add(movie)
+        movieAdapter.changeList(movieList)
+        Log.d("Number", movieList.size.toString())
+    }
+
+    private inner class FetchMovie : AsyncTask<String, Void, String>(){
+        override fun doInBackground(vararg params: String?): String {
+            if(params.isNullOrEmpty()) return ""
+
+            val movieName = params[0]
+            val movieUrl = NetworkUtils().buildtSearchUrl(movieName!!)
+            return try {
+                NetworkUtils().getResponseFromHttpUrl(movieUrl)
+            } catch (e: IOException){
+                ""
+            }
+        }
+
+        override fun onPostExecute(movieInfo: String) {
+            super.onPostExecute(movieInfo)
+            if (!movieInfo.isEmpty()){
+                val movieJson = JSONObject(movieInfo)
+                if(movieJson.getString("Response") == "True"){
+                    val movie = Gson().fromJson<Movie>(movieInfo, Movie::class.java)
+                    addMovieToList(movie)
+                } else{
+                    Snackbar.make(main_ll, "No existe la pelicula en la base", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
